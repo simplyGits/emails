simplyGits/emails

commit 91c98acebea422ea83d669ee2a6c52d7c4f42052

code cleanup and small changes

dev_server.js
@@ -1,19 +1,35 @@
-var email = require("simplyemail");
+'use strict';
+
+var email = require('simplyemail');
 var express = require('express');
+
+const PORT = 3000;
 var app = express();
 
 app.get('/cijfer', function (req, res) {
-	email.cijfer("Nederlands", "https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E", "https://app.simplyhomework.nl/settings", 9.2, 8.5,function(html){
-		res.send(html);
+	email.cijfer({
+		className: 'Nederlands',
+		classUrl: 'https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E',
+		settingsUrl: 'https://app.simplyhomework.nl/settings',
+		grade: '9,2',
+		passed: true,
+		average: '8,5'
+	}).then(function (res) {
+		res.end(res);
 	});
 });
 
 app.get('/project', function (req, res) {
-	email.project("Miljoenennota", "https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E", "https://app.simplyhomework.nl/settings", "Henk de Bakker", function(html){
-		res.send(html);
+	email.project({
+		projectName: 'Miljoenennota',
+		projectUrl: 'https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E',
+		settingsUrl: 'https://app.simplyhomework.nl/settings',
+		personName: 'Henk de Bakker'
+	}).then(function (res) {
+		res.end(res);
 	});
 });
 
-app.listen(3000, function () {
-	console.log('Dev server running on port 3000');
+app.listen(PORT, function () {
+	console.log('Dev server running on port', PORT);
 });

index.js
@@ -1,73 +1,86 @@
-var fs = require('fs');
-var email = require("simplyemail");
-var nodemailer = require('nodemailer');
-var tokens = require("./tokens.json");
+'use strict';
+
+const fs = require('fs');
+const email = require('simplyemail');
+const nodemailer = require('nodemailer');
+// const tokens = require('./tokens.json');
 
 /* To create unique subject so inbox doesn't bundle it. */
-function makeid(){
-		var text = "";
-		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function genId () {
+	let text = '';
 
-		for( var i=0; i < 5; i++ )
-				text += possible.charAt(Math.floor(Math.random() * possible.length));
+	for (let i = 0; i < 5; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
 
-		return text;
+	return text;
 }
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-var util = require('util');
+const util = require('util');
 
-console.log("Welke template verzenden?");
+console.log('Welke template verzenden?');
 process.stdin.on('data', function (text) {
 	//console.log('User gave input: ', util.inspect(text));
-	text = text.trim();
-	switch (text){
-		case "cijfer":
-			email.cijfer("Nederlands", "https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E", "https://app.simplyhomework.nl/settings", 5.4, 6.2).then(function(result){
-				sender(result, text);
-			}, function(error){
-				console.log(error);
-			});
-			break;
-		case "project":
-			email.project("Miljoenennota", "https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E", "https://app.simplyhomework.nl/settings", "Henk de Bakker").then(function(result){
-				sender(result, text);
-			}, function(error){
-				console.log(error);
-			});
-			break;
-		default:
-			console.log("template not found.");
-			process.exit();
+	text = text.replace(/\W/g, '');
+	if (text == 'cijfer') {
+		email.cijfer({
+			className: 'Nederlands',
+			classUrl: 'https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E',
+			settingsUrl: 'https://app.simplyhomework.nl/settings',
+			grade: '5,4',
+			passed: false,
+			average: '6,2',
+		}).then(function(result){
+			console.log(result);
+			// sender(result, text);
+		}, function(error){
+			console.log(error);
+		});
+	} else if (text == 'project') {
+		email.project({
+			projectName: 'Miljoenennota',
+			projectUrl: 'https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E',
+			settingsUrl: 'https://app.simplyhomework.nl/settings',
+			personName: 'Henk de Bakker',
+		}).then(function(result){
+			console.log(result);
+			// sender(result, text);
+		}, function(error){
+			console.log(error);
+		});
+	} else {
+		throw new Error('template not found');
 	}
 });
 
-function sender(result, template){
+function sender (result, template) {
 	/* Send email */
 	// create reusable transporter object using the default SMTP transport
 	// gmailUrl: smtps://xxxxx%40gmail.com:password@smtp.gmail.com
-	var transporter = nodemailer.createTransport(tokens.gmailUrl);
+	const transporter = nodemailer.createTransport(tokens.gmailUrl);
 
 	// setup e-mail data with unicode symbols
-	var mailOptions = {
+	const mailOptions = {
 		from: '"Thomas Konings" <tkon99@gmail.com>', // sender address
 		to: 'Thomas Konings, tkon99@gmail.com', // list of receivers
-		subject: 'sH test - '+template+' - '+makeid(), // Subject line
-		html: result // html body
+		subject: `sH test - ${template} - ${genId()}`,
+		html: result, // html body
 	};
 
 	// send mail with defined transport object
-	transporter.sendMail(mailOptions, function(error, info){
-		if(error){
-				return console.log(error);
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error != null) {
+			return console.log(error);
 		}
 		console.log('Message sent: ' + info.response);
 		process.exit();
 	});
 
 	/* Save code */
-	fs.writeFile('test.html', result, function(err){
+	fs.writeFile('test.html', result, function (err) {
 		if (err) throw err;
 		console.log('Code saved to test.html');
 	});

commit 64133aeae3b9bf92274d26bb1445e2c2a1db7193

Fixed dev_server bug caused by 91c98ac
Test your code m9 :P

dev_server.js
@@ -14,8 +14,8 @@ app.get('/cijfer', function (req, res) {
 		grade: '9,2',
 		passed: true,
 		average: '8,5'
-	}).then(function (res) {
-		res.end(res);
+	}).then(function (data) {
+		res.end(data);
 	});
 });
 
@@ -25,8 +25,8 @@ app.get('/project', function (req, res) {
 		projectUrl: 'https://app.simplyhomework.nl/class/YfDrLGoRfkRoqNe6E',
 		settingsUrl: 'https://app.simplyhomework.nl/settings',
 		personName: 'Henk de Bakker'
-	}).then(function (res) {
-		res.end(res);
+	}).then(function (data) {
+		res.end(data);
 	});
 });
 